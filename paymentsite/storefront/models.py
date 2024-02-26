476731f1be1fac7576fcafd01a22dec36bc2fe06
FrankExViews <123492696+FrankExViews@users.noreply.github.com>
2024-02-26 13:53:43 +0000

    stirpe db setup
    some issues, but table dropped and remigrated

--- paymentsite/storefront/models.py
+++ paymentsite/storefront/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class stripeorders(models.Model):
+    stripe_order_num = models.CharField(max_length=50,primary_key=True)
+    stripe_order_username = models.CharField(max_length=50)
+    stripe_ship_status = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.stripe_order_num,self.stripe_order_username,self.stripe_ship_status,
